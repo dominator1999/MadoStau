16f26f63c8b1f8f52d42a599912f41290d913a85
dominator1999 <aulinger.dominik@gmx.de>
2016-08-16 22:27:45 +0200

    #1- new layout

---
+++ app/src/main/java/madostau/madostau/CreateNewTravel.java
@@ -0,0 +1,17 @@
+package madostau.madostau;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dominikaulinger on 16.08.16.
+ */
+public class CreateNewTravel extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
--- app/src/main/java/madostau/madostau/MainActivity.java
+++ app/src/main/java/madostau/madostau/MainActivity.java
@@ -1,7 +1,10 @@
 package madostau.madostau;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +15,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void buttonOnClick(){
-        
-    }
+    public void buttonOnClick(View w) {
 
+        Button neuReise = (Button) findViewById(R.id.reiseNeu);
+        Button editReise = (Button) findViewById(R.id.editReise);
+        Button addFreund = (Button) findViewById(R.id.addFreund);
+        Button settings = (Button) findViewById(R.id.settings);
+
+        neuReise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CreateNewTravel.class);
+                startActivity(intent);
+            }
+        });
+
+        editReise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        addFreund.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
 }
