00555bc08a0cc8d165205288c287a829156aaeab
dominator1999 <aulinger.dominik@gmx.de>
2016-08-16 16:58:54 +0200

    initial commit #3

--- app/src/main/java/madostau/madostau/MainActivity.java
+++ app/src/main/java/madostau/madostau/MainActivity.java
@@ -2,6 +2,7 @@ package madostau.madostau;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonOnClick(){
-        
+        System.out.println("Bla");
     }
 
 }
